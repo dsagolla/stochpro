cabd9040881d701e766739e6d2c211031dfad2f8
dsagolla <dsagolla@mailbox.org>
2022-12-17 00:56:18 +0100

    Update __init__.py

--- src/stochpro/__init__.py
+++ src/stochpro/__init__.py
@@ -1,3 +1,15 @@
+"""
+StochPro
+########
+
+A Python package for sampling from stochastic processes.
+
+Classes
+-------
+GaussianProcess
+BrownianMotion
+ItoProcess
+"""
 from stochpro.gaussian import GaussianProcess
 from stochpro.brownian import BrownianMotion
 from stochpro.ito import ItoProcess
