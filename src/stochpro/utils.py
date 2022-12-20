11952c14967eb78c673748dd47541b45b636d477
dsagolla <dsagolla@mailbox.org>
2022-12-20 12:15:55 +0100

    Update utils.py

--- src/stochpro/utils.py
+++ src/stochpro/utils.py
@@ -1,12 +1,11 @@
 """Utility functions.
 """
-from typing import List
 from typing import Iterable
 
 import numpy as np
 
 
-def generate_times(end: float = 1.0, n: int = 10) -> List[float]:
+def generate_times(end: float, n: int) -> np.array:
     """Generate times.
 
     Generate a linspace from 0 to ``end`` for ``n`` increments. That is
@@ -15,15 +14,14 @@ def generate_times(end: float = 1.0, n: int = 10) -> List[float]:
 
     Parameters
     ----------
-    n : int, optional
-        The number of increments, by default 10.
-    end : float, optional
-        The right hand side of the interval :math:`[0,end]`, by default
-        1.0.
+    n : int
+        The number of increments.
+    end : float
+        The right hand side of the interval :math:`[0,end]`.
 
     Returns
     -------
-    List[float]
+    np.array
         A vector of times.
     """
     return np.linspace(0, end, n+1)
@@ -51,11 +49,16 @@ def check_times(times: Iterable[float]) -> None:
     ValueError
         Times are not nonnegative or not strictly increasing.
     """
-    if np.any([not isinstance(t, float)] for t in times):
+
+    if np.any([[not isinstance(t, float)] for t in times]):
         raise TypeError('Times must be an iterable of floats.')
+
     if np.any([t < 0 for t in times]):
         raise ValueError('Times must be nonnegative.')
+
     increments = np.diff(times)
+
     if np.any([t < 0 for t in increments]):
         raise ValueError('Times must be strictly increasing.')
+
     return None
